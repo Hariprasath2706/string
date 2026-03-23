8244ead931d3fa20bd51a2c81081dd7bf1747152
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-23 13:09:59 +0530

    Create Comparing strings.java

---
+++ Comparing strings.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        String txt1 = "Hello Prasath";
+        String txt2 = "Hello Hari";
+        
+        String txt3 = "Hello S";
+        String txt4 = "Hello S";
+        System.out.println(txt1.equals(txt2));
+        System.out.println(txt3.equals(txt4));
+    }
+}
