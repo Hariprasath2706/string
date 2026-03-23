05ef8aa40d5494128034f862870abfb1e718c9bb
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-23 13:18:45 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        String firstname = "Hari";
+        String lastname = "Prasath S";
+        System.out.println(firstname+" "+lastname);
+    }
+}
