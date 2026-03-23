faaf98e1b29d1e97928b4768f084fc268219f420
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-23 12:47:41 +0530

    Create String length.java

---
+++ String length.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        String txt = "ABCDEFGHIJKLMNOp";
+        System.out.println("The length of the txt is: " + txt.length());
+    }
+}
